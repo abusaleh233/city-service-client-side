4227e6554b874bcdf712863b8175e6bcc6327481
abusaleh233 <abusaleh233@gmail.com>
2025-11-13 12:18:26 +0600

    my issue card update

--- src/components/MyIssuesCard.jsx
+++ src/components/MyIssuesCard.jsx
@@ -6,7 +6,7 @@ const MyIssuesCard = ({issue}) => {
     const {title, category, location,amount, _id, image} = issue
 
     return (
-         <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2">
+         <div className="card p-4 bg-base-100 shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2">
             <figure className="h-48 overflow-hidden">
                 <img
                 src={image}
