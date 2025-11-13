63ebb32a8b85922dd9fecc742e9f026984007ea5
abusaleh233 <abusaleh233@gmail.com>
2025-11-13 12:16:14 +0600

    issueCard update

--- src/components/IssuesCard.jsx
+++ src/components/IssuesCard.jsx
@@ -7,9 +7,9 @@ const IssuesCard = ({issue}) => {
     const {title, category, location,description, _id} = issue
 
     return (
-    <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2">
-                <div className="">
-                    <h2 className="card-title text-cyan-600">{title}</h2>
+    <div className="card p-5 bg-base-100 shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2">
+                <div className="grid grid-cols-1 gap-3">
+                    <h2 className="card-title text-cyan-600 font-extrabold">{title}</h2>
                     <div className="font-bold text-sm ">{category}</div>
                     <div className="text-xs text-blue-700">{location}</div>
                     <p>{description}</p>
